bb866880adbde76a00bd2a8432739ad05c097a6b
greyhoundforty <ryantiffany@fastmail.com>
2019-12-02 10:51:24 -0600

    Testing with updated provider block
    Signed-off-by: greyhoundforty <ryantiffany@fastmail.com>

--- providers.tf
+++ providers.tf
@@ -1,7 +1,3 @@
 provider "ibm" {
-  ibmcloud_api_key   = "${var.ibm_bx_api_key}"
-  generation         = 1
-  region             = "eu-gb"
-  softlayer_username = "${var.ibm_sl_username}"
-  softlayer_api_key  = "${var.ibm_sl_api_key}"
+  generation = 1
 }
--- variables.tf
+++ variables.tf
@@ -1,17 +1,17 @@
-variable ibm_bx_api_key {
-  type    = "string"
-  default = ""
-}
+# variable ibm_bx_api_key {
+#   type    = "string"
+#   default = ""
+# }
 
-variable ibm_sl_username {
-  type    = "string"
-  default = ""
-}
+# variable ibm_sl_username {
+#   type    = "string"
+#   default = ""
+# }
 
-variable ibm_sl_api_key {
-  type    = "string"
-  default = ""
-}
+# variable ibm_sl_api_key {
+#   type    = "string"
+#   default = ""
+# }
 
 variable "ssh_key" {
   default = "tycho-eu-gb"
